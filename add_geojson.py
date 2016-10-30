36ad5e658ff89848bfb127352b1deceef062371a
Sebastian Schulz <schulz.siwy@gmail.com>
2016-10-31 00:25:53 +0100

    python3 13s

--- add_geojson.py
+++ add_geojson.py
@@ -3,9 +3,11 @@ from shapely.geometry import shape
 import json
 
 with open('static/v.geojson') as f:
-    geojson = json.load(f)
-
+    geojson = json.load(open('static/v.geojson'))
+data = []
+for feature in geojson['features']:
+    data.append({
+        'geometry': shape(feature['geometry']).wkt,
+        'attributes': feature['properties']})
 with db.atomic():
-    for feature in geojson['features']:
-        Features.create(geometry=shape(feature['geometry']).wkt,
-                        attributes=feature['properties'])
+    Features.insert_many(data).execute()
