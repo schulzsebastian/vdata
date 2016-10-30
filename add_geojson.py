1e120461b0dbc91b02da1a069029205c16a1d68f
Sebastian Schulz <schulz.siwy@gmail.com>
2016-10-30 17:18:10 +0100

    style fix

--- add_geojson.py
+++ add_geojson.py
@@ -1,14 +1,9 @@
 from app import *
-from peewee import fn
 from shapely.geometry import shape
 import json
 
-def read_geojson(filename):
-    with open(filename) as f:
-        return json.load(f)
-
-
-geojson = read_geojson('static/v.geojson')
+with open('static/v.geojson') as f:
+    geojson = json.load(f)
 for feature in geojson['features']:
     Features.create(geometry=shape(feature['geometry']).wkt,
                     attributes=feature['properties'])
