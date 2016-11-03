999270b32ff60f0f17cd13eb0c8b391069bcdfba
Sebastian Schulz <schulz.siwy@gmail.com>
2016-11-03 20:40:19 +0100

    python3 9s

--- add_geojson.py
+++ add_geojson.py
@@ -1,13 +1,22 @@
-from app import *
 from shapely.geometry import shape
+from io import StringIO
+import psycopg2
 import json
 
 with open('static/v.geojson') as f:
     geojson = json.load(open('static/v.geojson'))
-data = []
+data = StringIO()
 for feature in geojson['features']:
-    data.append({
-        'geometry': shape(feature['geometry']).wkt,
-        'attributes': feature['properties']})
-with db.atomic():
-    Features.insert_many(data).execute()
+    data.write(
+        '\t'.join([
+            shape(feature['geometry']).wkt,
+            json.dumps(feature['properties'])]) + '\n')
+data.seek(0)
+conn = psycopg2.connect("dbname='xx' \
+                        user='xx' \
+                        host='xx' \
+                        password='xx'")
+curs = conn.cursor()
+curs.copy_from(data, 'features', columns=('geometry', 'attributes'))
+conn.commit()
+conn.close()
